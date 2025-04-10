620c05bef25a07f5e9ed6f3e6816e2eb6fed2583
bsnlmahesh <mahesh.23joy@gmail.com>
2025-04-10 18:57:51 +0530

    python code for atm pin added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
-backend code added by ravi
-2nd line code added
-dSJDSDld'D
-lahflafhla;
+# backend code added by ravi
+# 2nd line code added
+# dSJDSDld'D
+# lahflafhla;
 
-rg added
+# rg added
 # 3rd line code added
+
+
+# code = 1234
+# num = int(input("Enter the code: "))
+# attempt=0
+# while num != code:
+#     print("incorrect code")
+#     attempt += 1
+#     if attempt == 3:
+#         print("Too many attempts")
+#         break
+#     num = int(input("Enter the code: "))
+   
+# else:
+#     print("welcome to falana bank")
+
